d7961a9e971115456559c88bc807fbbed0ab3849
Eric G <16710890+Pheric@users.noreply.github.com>
2019-04-03 18:35:32 -0500

    Enable firewall script

--- main.sh
+++ main.sh
@@ -4,8 +4,9 @@ function main {
   echo "MICS Security"
   su - root # so the user is prompted only once
 
-  chmod +x ./sysBackup.sh
+  chmod +x {sysBackup.sh,firewall.sh}
  ./sysBackup.sh
+ ./firewall.sh
 }
 
 main
