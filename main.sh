e7d35982e86fcad99c57c99fedf75ba69cc21f26
Eric G <16710890+Pheric@users.noreply.github.com>
2019-04-03 23:49:52 -0500

    Add code to check if pam.d/su was modified by red team

--- main.sh
+++ main.sh
@@ -9,11 +9,12 @@ function main {
     delCron # safe because cron jobs should be saved already. If this fails, we should have a system backup.
     ./firewall.sh
     ./sshdConfigure.sh
+    checkPamHack
 
     echo "Reminders:"
     echo "1. Disable root login through SSH (line 32)"
     echo "2. Check crontabs (/bak/etc/ and /bak/var/spool/cron/)"
-    echo "3. Check sudoers file"
+    echo "3. Check sudoers file and rc.local"
 }
 
 function delCron {
@@ -21,5 +22,20 @@ function delCron {
     rm -rf /var/spool/cron
 }
 
+function checkPamHack {
+    # making a simple change to the pam.d/su file can allow non-privileged users to escalate without a password.
+    # here we check for that and warn the admin if it might be an issue.
+
+    # hash made on my (Eric's) arch machine
+    safe="fa85e5cce5d723275b14365ba71a8aad  /etc/pam.d/su"
+    check="$(md5sum /etc/pam.d/su)"
+    if [ "$safe" != "$check" ]; then
+        echo "WARNING: The hash of /etc/pam.d/su does not match the predefined value!"
+    fi
+
+    if grep -i -q "permit" /etc/pam.d/su; then
+        echo "WARNING: SU HACK WAS FOUND IN /etc/pam.d/su"
+    fi
+}
 
 main
