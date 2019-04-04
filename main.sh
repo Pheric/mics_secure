81e418c153dbb66a9c3f3ceb08c47bae91a5d092
Eric G <16710890+Pheric@users.noreply.github.com>
2019-04-03 22:49:24 -0500

    Add script to configure sshd

--- main.sh
+++ main.sh
@@ -4,10 +4,11 @@ function main {
     echo "MICS Security"
     su - root # so the user is prompted only once
 
-    chmod +x {sysBackup.sh,firewall.sh,rclocalCompat.sh}
+    chmod +x *.sh
     ./sysBackup.sh
     delCron # safe because cron jobs should be saved already. If this fails, we should have a system backup.
     ./firewall.sh
+    ./sshdConfigure.sh
 
     echo "Reminders:"
     echo "1. Disable root login through SSH (line 32)"
@@ -19,5 +20,6 @@ function delCron {
     rm -rf /etc/cron*
     rm -rf /var/spool/cron
 }
+
 
 main
