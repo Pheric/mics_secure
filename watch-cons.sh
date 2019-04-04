a3597399d14e42e1deb4c3ed43e7c858d6cf7d50
Cras <christopher.loutsch@gmail.com>
2019-04-04 15:56:44 -0500

    Added a watch script

---
+++ watch-cons.sh
@@ -0,0 +1,4 @@
+netstat -nta | egrep "State|*" | sed "/\b\(127.0.0.1\)\b/d";
+python3 -c 'print("-"*100+"\n")';
+ss -nta "( dport = :* )" | sed "/\b\(127.0.0.1\)\b/d";
+python3 -c 'print("-"*100+"\n")'
