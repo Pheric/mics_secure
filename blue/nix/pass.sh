b43f3f458555d8880e403703451176caa0f6888e
Eric G <16710890+Pheric@users.noreply.github.com>
2019-09-15 21:02:07 -0500

    Make passwords unique for each user

--- blue/nix/pass.sh
+++ blue/nix/pass.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-	read -p : pass
-users=$(getent passwd | grep /bin/.*sh | grep -v root | cut -d ":" -f 1)
+
+# shellcheck disable=SC2162
+read -p "Enter password salt:" salt
+users=$(getent passwd | grep '/bin/.*sh' | grep -v root | cut -d ":" -f 1)
 for user in $users; do
-echo “$user:$pass” | chpasswd
-echo “$user:$pass” >> system_name.csv
+  password=$(echo "$user" | sha256sum | cut -c-7)$salt
+  echo "$user:$password" | chpasswd
 done
+
+echo "User passwords are now set to the first 7 chars of the username's sha256sum concatenated with the inputted salt"
+echo "Example: \"root:Password1!\" becomes \"root:53175bc$salt\""
